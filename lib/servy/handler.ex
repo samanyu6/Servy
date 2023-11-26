34095c6575ef61d2ddb775b33a5ccddb40266f1c
samanyu <samanyu973@gmail.com>
2023-11-26 17:25:47 +0530

    add header transformers

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,13 +7,18 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into a map:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
     # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
   end
 
   def format_response(conv) do
@@ -26,7 +31,6 @@ defmodule Servy.Handler do
     Bears, Lions, Tigers
     """
   end
-
 end
 
 request = """
@@ -39,4 +43,4 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
