49d2447a542677e2376240c2de97c6f74dcb2b4a
samanyu <samanyu973@gmail.com>
2023-11-26 18:01:55 +0530

    add function clauses

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,9 +2,12 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] =
@@ -17,8 +20,17 @@ defmodule Servy.Handler do
   end
 
   def route(request) do
+    route(request, request.method, request.path)
+  end
+
+  def route(request, "GET", "/wildthings") do
     # conv = Map.put(conv, :resp_body, "Bears)
     %{request | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(request, "GET", "/nicethings") do
+    # conv = Map.put(conv, :resp_body, "Bears)
+    %{request | resp_body: "Teddy Bears, Teddy Lions, Teddy Tigers"}
   end
 
   def format_response(request) do
