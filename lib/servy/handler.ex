18726136bfdff5e774291aeb14d867999d7134bc
samanyu <samanyu973@gmail.com>
2023-11-26 17:38:11 +0530

    complete parser

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,19 +16,19 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: ""}
   end
 
-  def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  def route(request) do
+    # conv = Map.put(conv, :resp_body, "Bears)
+    %{request | resp_body: "Bears, Lions, Tigers"}
   end
 
-  def format_response(conv) do
+  def format_response(request) do
     # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(request.resp_body)}
 
-    Bears, Lions, Tigers
+    #{request.resp_body}
     """
   end
 end
