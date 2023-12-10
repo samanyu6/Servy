25b0acb6a717e2c7888865addd8a542935176776
samanyu6 <samanyu973@gmail.com>
2023-12-10 13:54:56 +0530

    move render files to a sep function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -4,26 +4,27 @@ defmodule Servy.BearController do
 
   @templates_path Path.expand("../templates", __DIR__)
 
+  # default args \\
+  defp render(request, template, bindings \\ []) do
+    content = @templates_path
+    |> Path.join(template)
+    |> EEx.eval_file(bindings)
+
+    %{request | status: 200, resp_body: content}
+  end
+
   def index(request) do
     # all do the same thing
     bears = Wildthings.list_bears()
     |> Enum.sort(&Bear.order_asc_by_name(&1, &2))
 
-    content = @templates_path
-    |> Path.join("index.eex")
-    |> EEx.eval_file(bears: bears)
-
-    %{request | status: 200, resp_body: content}
+    render(request, "index.eex", bears: bears)
   end
 
   def show(request, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    content = @templates_path
-    |> Path.join("show.eex")
-    |> EEx.eval_file(bear: bear)
-
-    %{request | status: 200, resp_body: content}
+    render(request, "show.eex", bear: bear)
   end
 
   def create(request, %{"name" => name, "type" => type}) do
